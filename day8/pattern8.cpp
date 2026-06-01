63fb4886d688223bb54253ffc1d3ec773307a8f9
Dev Growth <projectwork1290@gmail.com>
2026-06-01 14:16:41 +0530

    Day-8

---
+++ day8/pattern8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<5;i++){
+        int start=1;
+        if(i%2==0)
+                start=1;
+            else
+                start=0;
+        for(int j=0;j<=i;j++){
+            cout << start;
+            start=1-start;
+        }
+        cout << endl;
+    }
+}
