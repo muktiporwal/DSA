aff71dda87265d57d74bea8d20cb55e874ad4e43
Dev Growth <projectwork1290@gmail.com>
2026-06-03 12:27:16 +0530

    Day-10

--- day10/occurOnce2.cpp
+++ day10/occurOnce2.cpp
@@ -21,13 +21,11 @@ public:
     }
 };
 int main(){
-    int n;
-    cout << "Enter the size of array: ";
-    cin >> n;
-    vector<int> arr(n);
-    cout << "Enter the elements of array: ";
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
+    vector<int> arr;
+    int x;
+    cout << "Enter the elements of array(use -11 to stop): ";
+    while(cin >> x && x!=-11){
+        arr.push_back(x);
     }
     Solution obj;
     cout << "Single element is: " << obj.singleElement(arr) << endl;
